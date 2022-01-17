beb004522561a29bd959df1de7ed4eb41ef7e477
mdzankic <matea.dzankic@fpmoz.sum.ba>
2022-01-17 11:52:53 +0100

    whatever

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
 	<title>Sportska oprema</title>
 	<meta charset="utf-8" />
 	
-	<link rel = "stylesheet" type = "text/css" href = "css/sAMA.css">
+	<link rel = "stylesheet" type = "text/css" href = "css/app.css">
 
 </head>
 
